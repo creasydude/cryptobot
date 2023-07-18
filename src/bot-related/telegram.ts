899678c8cd1731c338aab4dd969c2e3a63fc9312
creasydude <mohammadicecream@yahoo.com>
2023-07-18 15:10:38 +0330

    .

--- src/bot-related/telegram.ts
+++ src/bot-related/telegram.ts
@@ -5,32 +5,72 @@ const botToken = TELEGRAM_BOT_TOKEN!;
 const ownerChatId = TELEGRAM_OWNER_CHAT_ID!;
 const TBot = new TelegramBot(botToken, { polling: true });
 
-let isBotActive = false;
-
-function resetBotActive(mode: string) {
-    if (mode === "start") {
-        isBotActive = true;
-    } else if (mode === "stop") {
-        isBotActive = false;
-    }
-}
-
 TBot.onText(/\/start/, (msg) => {
-    if (!isBotActive) {
-        TBot.sendMessage(ownerChatId, "You Started Bot Successfully. ASAP You Will Receive Your Signals.");
-        resetBotActive("start");
+    const chatId = msg.chat.id;
+    // Check if the bot is already started
+    if (!isBotStarted(chatId)) {
+      // Perform start action
+      startBot(chatId);
+      TBot.sendMessage(chatId, 'Bot started!');
+    } else {
+        TBot.sendMessage(chatId, 'Bot already started!');
     }
-});
+  });
+  
+  TBot.onText(/\/stop/, (msg) => {
+    // const chatId = msg.chat.id;
+    const chatId = ownerChatId;
+    // Check if the bot is already stopped
+    if (isBotStarted(chatId)) {
+      // Perform stop action
+      stopBot(chatId);
+      TBot.sendMessage(chatId, 'Bot stopped!');
+    } else {
+        TBot.sendMessage(chatId, 'Bot already stopped!');
+    }
+  });
 
-TBot.onText(/\/stop/, (msg) => {
-    if (isBotActive) {
-        TBot.sendMessage(ownerChatId, "Bot activities stopped successfully.");
-        resetBotActive("stop");
-    }
-});
+  const botState : any = {};
+
+  function startBot(chatId : any) {
+    botState[chatId] = true;
+  }
+  
+  function stopBot(chatId : any) {
+    botState[chatId] = false;
+  }
+  
+  function isBotStarted(chatId : any) {
+    return botState[chatId] || false;
+  }
+    
+
+// let isBotActive = false;
+
+// function resetBotActive(mode: string) {
+//     if (mode === "start") {
+//         isBotActive = true;
+//     } else if (mode === "stop") {
+//         isBotActive = false;
+//     }
+// }
+
+// TBot.onText(/\/start/, (msg) => {
+//     if (!isBotActive) {
+//         TBot.sendMessage(ownerChatId, "You Started Bot Successfully. ASAP You Will Receive Your Signals.");
+//         resetBotActive("start");
+//     }
+// });
+
+// TBot.onText(/\/stop/, (msg) => {
+//     if (isBotActive) {
+//         TBot.sendMessage(ownerChatId, "Bot activities stopped successfully.");
+//         resetBotActive("stop");
+//     }
+// });
 
 function BotSendMsg(message: string) {
-    if (isBotActive) {
+    if (isBotStarted(ownerChatId)) {
         TBot.sendMessage(ownerChatId, message);
     }
 }
